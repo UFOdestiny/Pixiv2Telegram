9afebfff03ddc03b43840c4d2a9bc9e0123f8767
UFOdestiny <1976438440@qq.com>
2022-09-02 14:17:24 +0800

    v1.0

---
+++ config.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Name     : pixiv.py
+# @Date     : 2022/9/2 13:26
+# @Auth     : UFOdestiny
+# @Desc     : config
+
+import platform
+
+
+class TelegramAccount:
+    api_id = 1234455
+    api_hash = 'abcde'
+
+    plat = platform.system().lower()
+    if plat == 'windows':
+        proxy = dict(scheme="http", hostname="127.0.0.1", port=7890)
+    elif plat == 'linux':
+        proxy = dict(hostname="127.0.0.1", port=7891)
+
+
+class PixivAccount:
+    token = "abcde"
+    proxy = {'http': 'http://127.0.0.1:7890', 'https': 'http://127.0.0.1:7890', }
+    userid = 12345
+
+    json_name = "cache.json"
+
+
+class LogSetting:
+    path = "log"
+
+
+if __name__ == "__main__":
+    print(TelegramAccount.proxy)
